f53161d1e9f86737b5ae62fc1c3733c361146471
Hyundoo, Jin <gusen0927@gmail.com>
2018-11-14 10:05:30 +0900

    First exp

---
+++ Naver_kin/Model/char_kor.py
@@ -0,0 +1,10 @@
+import requests
+from bs4 import BeautifulSoup
+import time
+from datetime import datetime
+import re
+from tqdm import tqdm, trange
+import tensorflow as tf
+import numpy as np
+import pandas as pd
+import pickle
